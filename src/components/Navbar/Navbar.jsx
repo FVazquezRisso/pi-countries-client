004e3bb37576205e2246ae322cafb133795ef027
FVazquezRisso <facundovazquez321@gmail.com>
2023-12-17 22:24:01 -0300

    Se modificó el componente NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import {
   removeCountriesId,
   setAllowedAccess,
   getAllActivities,
+  getAllUsers,
 } from "../../redux/actions";
 
 export default function Navbar() {
@@ -22,17 +23,20 @@ export default function Navbar() {
     dispatch(getAllActivities());
   };
 
+  const handleUsers = () => {
+    dispatch(getAllUsers());
+  };
+
   return (
     <NavContainer>
-      <NavButton
-        to={`/home/${UserId}`}
-        className={({ isActive }) => (isActive ? "isActive" : "")}
-        onClick={handleClick}
-      >
+      <NavButton to={`/home/${UserId}`} onClick={handleClick}>
         Home
       </NavButton>
       <NavButton to={"/activities"} onClick={handleActivities}>
         Activities
+      </NavButton>
+      <NavButton to={"/users"} onClick={handleUsers}>
+        Users
       </NavButton>
       <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
     </NavContainer>
